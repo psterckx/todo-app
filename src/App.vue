7a6a8cee4d6b95d6bdfcab2047c028baf7ffe1b3
Peter Sterckx <pasterckx@gmail.com>
2022-01-28 08:48:19 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="mx-72 my-12">
+    <h1 class="text-xl mb-6">My Tasks</h1>
+    <Task
+      v-for="task in tasks"
+      :key="task.id"
+      :id="task.id"
+      :name="task.name"
+      :isComplete="task.isComplete"
+      @toggleCompleteTask="toggleCompleteTask($event)"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Task from "./components/Task.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: "1643376797940",
+          name: "Make coffee",
+          isComplete: false,
+        },
+        {
+          id: "1643376801982",
+          name: "Hack",
+          isComplete: true,
+        },
+        {
+          id: "1643376814350",
+          name: "Make more coffee",
+          isComplete: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleCompleteTask(taskId) {
+      console.log(taskId);
+      const taskToUpdate = this.tasks.find((task) => task.id === taskId);
+      taskToUpdate.isComplete = !taskToUpdate.isComplete;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex items-center my-2">
+    <input type="checkbox" :checked="isComplete" @change="toggleCompleteTask" />
+    <div v-if="!isComplete" class="text-lg ml-2">{{ name }}</div>
+    <div v-else class="text-lg text-gray-500 ml-2">
+      <s>
+        {{ name }}
+      </s>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    id: String,
+    name: String,
+    isComplete: Boolean,
+  },
+  methods: {
+    toggleCompleteTask() {
+      this.$emit("toggleCompleteTask", this.id);
+    },
+  },
+};
+</script>
