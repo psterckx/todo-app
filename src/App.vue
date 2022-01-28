1405dc2fd70fcdf1a22a747599c2f2743c90b251
Peter Sterckx <pasterckx@gmail.com>
2022-01-28 09:14:07 -0500

    Finish get and update operations

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app" class="mx-72 my-12">
-    <h1 class="text-xl mb-6">My Tasks</h1>
-    <Task
-      v-for="task in tasks"
-      :key="task.id"
-      :id="task.id"
-      :name="task.name"
-      :isComplete="task.isComplete"
-      @toggleCompleteTask="toggleCompleteTask($event)"
-    />
+    <div class=" mb-6 flex items-center">
+      <h1 class="text-xl mr-8">My Tasks</h1>
+      <div v-if="isError">&#129301; Something's not right...</div>
+    </div>
+    <template>
+      <Task
+        v-for="task in tasks"
+        :key="task.id"
+        :id="task.id"
+        :name="task.name"
+        :isComplete="task.isComplete"
+        @toggleCompleteTask="toggleCompleteTask($event)"
+      />
+    </template>
   </div>
 </template>
 
 <script>
 import Task from "./components/Task.vue";
+import axios from "axios";
 
 export default {
   name: "App",
@@ -22,30 +28,48 @@ export default {
   },
   data() {
     return {
-      tasks: [
-        {
-          id: "1643376797940",
-          name: "Make coffee",
-          isComplete: false,
-        },
-        {
-          id: "1643376801982",
-          name: "Hack",
-          isComplete: true,
-        },
-        {
-          id: "1643376814350",
-          name: "Make more coffee",
-          isComplete: false,
-        },
-      ],
+      tasks: [],
+      isError: false,
     };
   },
+  created() {
+    this.getTasks();
+  },
   methods: {
-    toggleCompleteTask(taskId) {
-      console.log(taskId);
+    async getTasks() {
+      try {
+        const response = await axios.get(
+          "https://10hivotppf.execute-api.us-east-1.amazonaws.com/dev"
+        );
+        this.tasks = response.data.todos;
+      } catch (error) {
+        console.log(error);
+        this.isError = true;
+        this.tasks = [];
+      }
+    },
+    async updateTask(task) {
+      const response = await axios.post(
+        "https://10hivotppf.execute-api.us-east-1.amazonaws.com/dev",
+        task
+      );
+      this.tasks = this.tasks.map((task) => {
+        if (task.id === response.data.id) {
+          return response.data;
+        } else {
+          return task;
+        }
+      });
+    },
+    async toggleCompleteTask(taskId) {
       const taskToUpdate = this.tasks.find((task) => task.id === taskId);
       taskToUpdate.isComplete = !taskToUpdate.isComplete;
+      try {
+        await this.updateTask(taskToUpdate);
+      } catch (error) {
+        this.isError = true;
+        taskToUpdate.isComplete = !taskToUpdate.isComplete;
+      }
     },
   },
 };
